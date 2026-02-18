b63457a13cfed9eea5c383a0778c0aac1d90d1d0
baptz12 <baptiste.zetler@epitech.eu>
2026-02-18 14:54:53 +0100

    [FEAT]: Add muzzle flash when shooting, crosshair and first enemy

--- Brackeys-jam/scripts/player.gd
+++ Brackeys-jam/scripts/player.gd
@@ -8,9 +8,18 @@ const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
 
+# Variables pour le Screen Shake
+var shake_strength: float = 0.0
+@export var shake_decay: float = 5.0  # Vitesse à laquelle la secousse s'arrête
+
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
 @onready var spot_light_3d: SpotLight3D = $Head/Camera3D/Spotlight/SpotLight3D
+
+# Récupération des nœuds du flash
+@onready var flash_sprite: Sprite3D = $Head/Camera3D/Gun/Muzzle/Sprite3D
+@onready var flash_light: OmniLight3D = $Head/Camera3D/Gun/Muzzle/OmniLight3D
+@onready var gpu_particles_3d: GPUParticles3D = $Head/Camera3D/Gun/Muzzle/GPUParticles3D
 
 
 func _ready() -> void:
@@ -29,6 +38,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 	if event.is_action_pressed("flashlight"):
 		switch_flashlight()
+		
+	if event.is_action_pressed("shoot"):
+		create_muzzle_flash()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -44,7 +56,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction : Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var new_speed = 0
-	
+		
 	if Input.is_action_pressed(	"sprint"):
 		new_speed = 10
 	
@@ -57,10 +69,55 @@ func _physics_process(delta: float) -> void:
 		
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
+	var bob_vector = _headbob(t_bob)
+	
+	var shake_vector = Vector3.ZERO
+	if shake_strength > 0:
+		# On réduit la force petit à petit
+		shake_strength = lerp(shake_strength, 0.0, shake_decay * delta)
+		
+		# On crée un vecteur de déplacement aléatoire
+		shake_vector = Vector3(
+			randf_range(-shake_strength, shake_strength),
+			randf_range(-shake_strength, shake_strength),
+			0.0
+		)
+	
+	camera.transform.origin = bob_vector + shake_vector
+	
+	# On s'assure que les offsets sont à 0 (on n'utilise plus ça)
+	camera.h_offset = 0.0
+	camera.v_offset = 0.0
 
+		
 	move_and_slide()
 
+func create_muzzle_flash() -> void:
+	
+	apply_shake(0.1)
+	
+	gpu_particles_3d.restart()
+	gpu_particles_3d.emitting = true
+	
+	# 1. On rend visible
+	flash_sprite.visible = true
+	flash_light.visible = true
+	
+	# 2. Petite rotation aléatoire pour varier le visuel (Roll)
+	flash_sprite.rotation.z = randf_range(0, 2 * PI)
+	
+	# 3. On attend un tout petit moment (ex: 0.05 secondes)
+	# L'utilisation de 'create_timer' avec 'await' est parfaite pour ça
+	await get_tree().create_timer(0.1).timeout
+	
+	# 4. On cache tout
+	flash_sprite.visible = false
+	flash_light.visible = false
+	
+func apply_shake(strength: float) -> void:
+	# On définit la for	ce actuelle (ex: 0.1 pour un petit tir, 0.5 pour une explosion)
+	shake_strength = strength
+	
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
